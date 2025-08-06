f5a2cbe759da94c2ff57df1d7f7886815a5b6b6f
lhendupdamchey321 <lhendupdamchey@gmail.com>
2025-08-06 23:05:17 +0530

    booleans.py

--- csf101-python_exercises/basics/booleans.py
+++ csf101-python_exercises/basics/booleans.py
@@ -1,4 +1,9 @@
-is_python_fun = True
-print("python is fun:", is_python_fun)
-is_java_fun = False
-print("java is fun:", is_java_fun)
+is_student = True
+is_employed = False
+print(is_student, is_employed)
+
+is_student_or_employed = is_student or is_employed
+print(is_student_or_employed)
+
+is_student_or_employed = is_student or is_employed
+print(is_student_or_employed)
