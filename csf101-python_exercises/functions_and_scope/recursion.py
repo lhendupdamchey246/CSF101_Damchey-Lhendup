7fe41802b5a6d5fd7ebcc652fbf59851523ec57d
lhendupdamchey321 <lhendupdamchey@gmail.com>
2025-08-16 22:24:37 +0530

    recursion.py

---
+++ csf101-python_exercises/functions_and_scope/recursion.py
@@ -0,0 +1,23 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print(factorial(5))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 1)
+    
+print(fibonacci(5))
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 1)
+    
+print(fibonacci(7))
